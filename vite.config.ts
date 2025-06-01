d9ea8769df65523745f02e38c1d8ea8e6c3e5803
Maggie Ellinwood <mellinwood1023@gmail.com>
2025-05-31 20:27:05 -0400

    update port

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000, // Set a specific port for the development server
+  },
   build: {
     rollupOptions: {
       external: ['/vite.svg'],
